dbbf5aa3b76fd8fe1226f2fad80c85e1c7ad00ac
Anastasia <109382571+NastiaDuliba@users.noreply.github.com>
2023-11-14 18:57:26 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//let a =  parseFloat(prompt("введіть значення першого числа", "0"));
+//let b =  parseFloat(prompt("введіть значення другого числа", "0"));
+
+//let firstResult = a + b;
+//let twoResult = a / b;
+//console.log(firstResult)
+//console.log(twoResult);
